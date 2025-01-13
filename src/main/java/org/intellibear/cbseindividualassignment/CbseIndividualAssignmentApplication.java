5f68c354760a8f5ad92b953b7f927a1646542e6f
CHOO XIANG LUN <xlchoo25@gmail.com>
2025-01-13 23:01:30 +0800

    Initial Commit

---
+++ src/main/java/org/intellibear/cbseindividualassignment/CbseIndividualAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.intellibear.cbseindividualassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbseIndividualAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbseIndividualAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/intellibear/cbseindividualassignment/CbseIndividualAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intellibear.cbseindividualassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbseIndividualAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
